dfb50670051ce06944761eebc533155494b8a870
Stephen R. Aylward <stephen.aylward@kitware.com>
2015-03-16 00:32:48 -0400

    BUG: Bump Slicer to fix ITKVTK lib error in FACTORY_MANAGER.

--- CMake/Superbuild/External_Slicer.cmake
+++ CMake/Superbuild/External_Slicer.cmake
@@ -80,7 +80,7 @@ if(NOT DEFINED ${proj}_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
     set(${proj}_SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj})
     list(APPEND ${proj}_EP_ARGS
       GIT_REPOSITORY "${git_protocol}://github.com/KitwareMedical/Slicer.git"
-      GIT_TAG "55c3f965e4e03201206608a8d4d514e5f05fb73f"
+      GIT_TAG "61e8989766e6eee2a78426622178197178cfd312"
       )
   endif()
 
